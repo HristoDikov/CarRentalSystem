8525e7e070eeb390967d2225a401c73f83c95594
Hristo Dikov <dikoww7@gmail.com>
2021-01-23 17:51:41 +0200

    Added dealer domain model

---
+++ CarRentalSystem.Domein/Models/Dealer/Dealer.cs
@@ -0,0 +1,38 @@
+﻿namespace CarRentalSystem.Domain.Models.Dealer
+{
+    using CarAds;
+    using Common;
+    using Exceptions;
+    using System.Collections.Generic;
+
+    using static ModelConstants.Common;
+
+    public class Dealer : Entity<int>, IAggregateRoot
+    {
+        private readonly HashSet<CarAd> carAds;
+
+        public Dealer(string name, string phoneNumber)
+        {
+            this.Validate(name);
+
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+
+            this.carAds = new HashSet<CarAd>();
+        }
+
+        public string Name { get; }
+
+        public PhoneNumber PhoneNumber { get; }
+
+        private void Validate(string name)
+        {
+            Guard.ForStringLength<InvalidDealerException>(
+                name,
+                MinNameLength,
+                MaxNameLength,
+                nameof(this.Name));
+        }
+
+    }
+}
---
+++ CarRentalSystem.Domein/Models/Dealer/PhoneNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace CarRentalSystem.Domain.Models.Dealer
+{
+    using Exceptions;
+    using Common;
+
+    using static ModelConstants.PhoneNumber;
+
+    public class PhoneNumber : ValueObject
+    {
+        internal PhoneNumber(string number)
+        {
+            this.Validate(number);
+
+            if (!number.StartsWith(PhoneNumberFirstSymbol))
+            {
+                throw new InvalidPhoneNumberException($"Phone number must start with a '{PhoneNumberFirstSymbol}'.");
+            }
+        }
+
+
+        private void Validate(string phoneNumber)
+      => Guard.ForStringLength<InvalidPhoneNumberException>(
+          phoneNumber,
+          MinPhoneNumberLength,
+          MaxPhoneNumberLength,
+          nameof(PhoneNumber));
+    }
+}
